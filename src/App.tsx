622d38e30f0848eecf12b0dbc524dad772e0a6a1
Maximilian <scientificresearch309@gmail.com>
2025-09-02 20:02:22 +0200

    the useeffect and fetch are working well together

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,44 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 const employeeUrl = 'https://edwardtanguay.vercel.app/share/employees.json';
 
-// using IIFE => Immediately Invoked Function Expression
-// async () => {
-//   const data = await fetch(employeeUrl);
-//   console.log(data);
-// };
-
-const fetchEmployeeData = async () => {
-  const data = await fetch(employeeUrl);
-  const response = await data.json();
-  console.log(response);
-};
-
-fetchEmployeeData();
+interface Employee {
+  employeeID: number;
+  firstName: string;
+  lastName: string;
+  title: string;
+}
 
 function App() {
+  const [fetchData, setFetchData] = useState<Employee[]>([]);
+
+  // const fetchEmployeeData = async () => { Das führt dazu, dass die Funktion bei jedem Render erneut ausgeführt wird — also eine Endlosschleife von Fetches!
+
+  // useEffect(() => {}, []) sorgt dafür, dass der Fetch nur einmal beim Laden ausgeführt wird.
+  useEffect(() => {
+    const fetchEmployeeData = async () => {
+      const response = await fetch(employeeUrl);
+      const data = await response.json();
+      setFetchData(data);
+      console.log(fetchData);
+    };
+    fetchEmployeeData();
+  }, []);
+
   return (
     <div className="App">
       <h1>useEffect-Intro-React-Vite-Typescript</h1>
+      <div className="employees">
+        {fetchData.map((employee) => (
+          <div className="employee">
+            <p key={employee.employeeID} className="firstAndLastName">
+              {employee.firstName} {employee.lastName}
+            </p>
+            <p className="title">{employee.title}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
