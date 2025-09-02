413d9cee543a6deac37c60ac9028d31d8bb27ca0
Maximilian <scientificresearch309@gmail.com>
2025-09-02 13:09:57 +0200

    fetching the employee data using fetch() function in Browser - it works well

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,20 @@ import './App.css';
 
 const employeeUrl = 'https://edwardtanguay.vercel.app/share/employees.json';
 
+// using IIFE => Immediately Invoked Function Expression
+// async () => {
+//   const data = await fetch(employeeUrl);
+//   console.log(data);
+// };
+
+const fetchEmployeeData = async () => {
+  const data = await fetch(employeeUrl);
+  const response = await data.json();
+  console.log(response);
+};
+
+fetchEmployeeData();
+
 function App() {
   return (
     <div className="App">
